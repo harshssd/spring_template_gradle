610c052491b16b6021202a70702db1462e09a731
Harsha <somash@amazon.com>
2017-06-04 16:22:12 -0700

    Configure to run the Spring Application.

---
+++ src/main/java/com/harshssd/spring_template_gradle/AppConfig.java
@@ -0,0 +1,13 @@
+package com.harshssd.spring_template_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppConfig
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
