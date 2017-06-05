97cc40b9b17fc2114718f2870784f956d8b58e1e
Harsha <somash@amazon.com>
2017-06-04 18:23:07 -0700

    Add Thymeleaf dependency and render a html view.

--- src/main/java/com/harshssd/spring_template_gradle/controller/HomeController.java
+++ src/main/java/com/harshssd/spring_template_gradle/controller/HomeController.java
@@ -2,15 +2,13 @@ package com.harshssd.spring_template_gradle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class SampleController
+public class HomeController
 {
     @RequestMapping(value = "/")
-    @ResponseBody
-    public String helloSpring()
+    public String renderHome()
     {
-        return "Hello from Spring Starter Template using Gradle.";
+        return "home";
     }
 }
